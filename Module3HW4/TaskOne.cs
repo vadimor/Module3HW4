e276ce5c764e4b1de4c86d8e7ad193aaa6e8402a
vadimor <vadimorbiluy@gmail.com>
2021-12-16 20:08:13 +0200

    Add TaskOne

---
+++ Module3HW4/TaskOne.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Module3HW4
+{
+    public class TaskOne
+    {
+        public event Func<int, int, int> SumEvent;
+
+        public int SumResult(int x, int y)
+        {
+            SumEvent += Sum;
+            SumEvent += Sum;
+            var sum = 0;
+            foreach (var item in SumEvent.GetInvocationList())
+            {
+                sum += (int)item.DynamicInvoke(x, y);
+            }
+
+            return sum;
+        }
+
+        public void TryCatch(Action a)
+        {
+            try
+            {
+                a.Invoke();
+            }
+            catch
+            {
+            }
+        }
+
+        private int Sum(int x, int y) => x + y;
+    }
+}
